2c43f9efaa0c462e26891a5ef5792ef97a387ad1
junior llerena <th3julius@gmail.com>
2024-04-15 23:44:26 -0500

    nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-     <header>
-          <nav className='container__nav'>
-            <a href="#">Logo</a>
-            <ul className='container__lista'>
-              <li className='lista' ><a href="#">Inicio</a></li>
-              <li className='lista' ><a href="#">Sobre Mi</a></li>
-              <li className='lista' ><a href="#">Habilidades</a></li>
-              <li className='lista' ><a href="#">Mis proyectos</a></li>
-              <button className='button'>Contactame</button>
+      <div className="contenedor">
+        <header>
+          <nav>
+            <a className="logo">Logo</a>
+            <ul className="lista">
+              <li className="item">
+                <a >Inicio</a>
+              </li>
+              <li className="item">
+                <a >Sobre Mi</a>
+              </li>
+              <li className="item">
+                <a >Habilidades</a>
+              </li>
+              <li className="item">
+                <a >Mis proyectos</a>
+              </li>
+              
             </ul>
+            <button className="button">Contactame</button>
           </nav>
-     </header>
+        </header>
 
-
-     
-     <section>
-      <div className="container__information">
-            <div className='texto'>
-              <h2>Hola! soy Gerardo un <span>Ingeniero de Software</span></h2>
-              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae ex numquam odio tenetur esse neque hic aliquid, alias eveniet veniam dolores minus voluptas. Odit dignissimos reiciendis vel libero sed praesentium.</p>
+        <section>
+          <div className="container__information">
+            <div className="texto">
+              <h2>
+                Hola! soy Gerardo un <span>Ingeniero de Software</span>
+              </h2>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Repudiandae ex numquam odio tenetur esse neque hic aliquid,
+                alias eveniet veniam dolores minus voluptas. Odit dignissimos
+                reiciendis vel libero sed praesentium.
+              </p>
             </div>
-            <div className='imagen'>
+            <div className="imagen">
               <img src="" alt="Gerardo Josias Chevarria Rodriguez" />
             </div>
           </div>
-     </section>
+        </section>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
